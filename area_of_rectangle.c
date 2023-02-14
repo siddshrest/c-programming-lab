480058e1b80769b8b79b412bfba7fafe3557bfcd
siddshrest <125288395+siddshrest@users.noreply.github.com>
2023-02-14 08:24:05 +0545

    Update area_of_rectangle.c

--- area_of_rectangle.c
+++ area_of_rectangle.c
@@ -9,13 +9,21 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <stdio.h>
 void main()
 {
+
+    // Variable decleration
     int l , b , a , p;
+    
+    // Take input from the user
     printf("enter length and breadth");
     scanf("%d,%d",&l,&b);
+    
+    // Calculation
     a = l*b;
     p = 2*(l+b);
+    
+    //show result
     printf("area = %d \n", a);
-        printf("perimeter = %d", p);
+    printf("perimeter = %d", p);
 }
 
 
