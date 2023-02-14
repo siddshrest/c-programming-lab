d2aea7e83fa473440e6b665d4a9e9a8cf86fa5fb
siddshrest <125288395+siddshrest@users.noreply.github.com>
2023-02-14 08:10:07 +0545

    Create sq_root.c

---
+++ sq_root.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+void main()
+{
+    float a,b;
+    printf ("enter a num");
+    scanf ("%f",&a);
+    b = sqrt(a);
+    printf("sq root is %f",b);
+    getch();
+}
