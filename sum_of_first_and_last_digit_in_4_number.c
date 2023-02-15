b17f665cd2ec0a9958c1af89bfe52d16e51d9b7a
siddshrest <125288395+siddshrest@users.noreply.github.com>
2023-02-15 20:47:18 +0545

    Update sum_of_first_and_last_digit_in_4_number.c

--- sum_of_first_and_last_digit_in_4_number.c
+++ sum_of_first_and_last_digit_in_4_number.c
@@ -1,11 +1,16 @@
 /******************************************************************************
-
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
 *******************************************************************************/
-//wap to find sum of first and last digit in 4 number
+/******************************************************************************
+* Siddhartha Shrestha
+* C-programming
+* Program 2
+* 
+* Program to find sum of first and last digit in 4 number
+* 
+*******************************************************************************/
 
 #include <stdio.h>
 #include <conio.h>
@@ -13,20 +18,20 @@ Write your code in this editor and press "Run" button to compile and execute it.
 void main()
 {
     // variable decleration
-    int x,a,s,t,y,r,r2,sum;
+    int number, thousands, hundred_remainder, hundredth, tens_remainder, tens, ones_remainder, sum;
     
     //input from user
-    printf ("enter 4 digit number");
-    scanf ("%d",&a);
+    printf ("enter 4 digit number: \n");
+    scanf ("%d", &number); // 1234
     
-    // calculations
-    x = a/1000;
-    r = a%1000;
-    s = r/100;
-    r2 = r%100;
-    t = r2/10;
-    y = r2%10;
-    sum = x+y;
+    // calculations Sample run : 1234
+    thousands = number / 1000; //1                                   Int/Int = Int D/I= D I/D =D
+    hundred_remainder = number % 1000; // 234
+    hundredth = hundred_remainder / 100; // 2
+    tens_remainder = hundred_remainder % 100; // 34
+    tens = tens_remainder / 10;  // 3
+    ones_remainder = tens_remainder % 10; //4
+    sum = thousands + ones_remainder; // 1 + 4 = 5
     
     //result
     printf ("sum of first and last digit of 4 digit number is = %d",sum);
